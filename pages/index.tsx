829646f67e82ba6f145752e8d4ebab7365db5715
matej.vykoukal248@gmail.com <matej.vykoukal248@gmail.com>
2022-11-27 14:28:23 +0100

    feat: first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div>
+			<p>Press arrows to navigate between items.</p>
+			<p>Press enter to activate items.</p>
+			<nav>
+				<button>Alert me!</button>
+				<button>Chabge background</button>
+			</nav>
+		</div>
+	);
+}
